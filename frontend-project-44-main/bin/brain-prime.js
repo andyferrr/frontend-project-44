40c4436ace610799a81720da9afc2dbdb345405a
Andy Fedunin <andy.fedunin@gmail.com>
2026-06-10 11:44:52 +0300

    Удалить все файлы

--- frontend-project-44-main/bin/brain-prime.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import runPrimeGame from '../src/games/prime.js';
-runPrimeGame();
--- frontend-project-44-main/bin/brain-progression.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import runProgressionGame from '../src/games/progression.js';
-runProgressionGame();
